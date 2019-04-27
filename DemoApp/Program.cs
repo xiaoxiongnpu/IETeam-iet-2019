1d46222f7eaf4e8391f48082baf91ee1ab29d900
noramurakozy <murakozy.nora@gmail.com>
2019-04-27 19:01:03 +0200

    DemoApp created with a code sample

---
+++ DemoApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using RDFSharp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Code sample 1: Creating an RDFGraph with 3 triples, then write them to console.
+            var rdf = new RDFGraph(new List<RDFTriple>() {
+                new RDFTriple(new RDFResource("http://example.org/book/book1"), new RDFResource("http://purl.org/dc/elements/1.1/title"), new RDFPlainLiteral("SPARQL Tutorial")),
+                new RDFTriple(new RDFResource("http://example.org/book/book2"), new RDFResource("http://purl.org/dc/elements/1.1/title"), new RDFPlainLiteral("RDF book")),
+                new RDFTriple(new RDFResource("https://wwww.w3.org/"), new RDFResource("http://purl.org/dc/elements/1.1/title"), new RDFPlainLiteral("World Wide Web Consortium")),
+            });
+
+            foreach(var triple in rdf)
+            {
+                Console.WriteLine(triple.ToString()); 
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
