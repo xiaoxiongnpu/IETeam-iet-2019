8e7929332266e14065946c40dd9c63e30d4558f8
Sándor Bálint <balint245@gmail.com>
2019-05-08 13:39:41 +0200

    collectionunittests added

--- DemoApp/RDFComponentTests/ModelTests/CollectionUnitTests.cs
+++ DemoApp/RDFComponentTests/ModelTests/CollectionUnitTests.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace DemoApp.RDFComponentTests.ModelTests
 {
-    class CollectionUnitTests
+    public static class CollectionUnitTests
     {
+        [Fact]
+        public void AddResourceItemTest()
+        {
+            var collection = new RDFCollection(RDFModelEnums.RDFItemTypes.Resource);
+            var resource = new RDFResource("https://www.index.hu");
+            collection.AddItem(resource);
+            Assert.Equal(1, collection.ItemsCount);
+        }
+
+        [Fact]
+        public void AddLiteralItemTest()
+        {
+            var collection = new RDFCollection(RDFModelEnums.RDFItemTypes.Literal);
+            var literal = new RDFLiteral("https://www.index.hu");
+            collection.AddItem(literal);
+            Assert.Equal(1, collection.ItemsCount);
+        }
     }
 }
