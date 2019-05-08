203f995573d2ac48f3ea717279fc86173581666f
Sándor Bálint <balint245@gmail.com>
2019-05-08 13:54:24 +0200

    collectionunittests implemented

--- DemoApp/RDFComponentTests/ModelTests/CollectionUnitTests.cs
+++ DemoApp/RDFComponentTests/ModelTests/CollectionUnitTests.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RDFSharp.Model;
+using Xunit;
 
 namespace DemoApp.RDFComponentTests.ModelTests
 {
     public static class CollectionUnitTests
     {
+        #region AddTests
+        [Fact]
+        public void AddNullTest()
+        {
+            var collection = new RDFCollection(RDFModelEnums.RDFItemTypes.Resource);
+            RDFResource resource = null;
+            collection.AddItem(resource);
+            Assert.Equal(1, collection.ItemsCount);
+        }
+
         [Fact]
         public void AddResourceItemTest()
         {
@@ -25,5 +37,65 @@ namespace DemoApp.RDFComponentTests.ModelTests
             collection.AddItem(literal);
             Assert.Equal(1, collection.ItemsCount);
         }
+
+        [Fact]
+        public static void EmptyCollectionTest()
+        {
+            var collection = new RDFCollection(RDFModelEnums.RDFItemTypes.Resource);
+            Assert.Equal(0, collection.ItemsCount);
+        }
+
+        [Fact]
+        public static void DuplicatedItemsTest()
+        {
+            var collection = new RDFCollection(RDFModelEnums.RDFItemTypes.Resource);
+            var item1 = new RDFResource("https://index.hu");
+            var item2= new RDFResource("https://index.hu");
+
+            collection.AddItem(item1);
+            collection.AddItem(item2);
+            Assert.Equal(1, collection.ItemsCount);
+        }
+        #endregion
+
+        #region RemoveTests
+        [Fact]
+        public static void RemoveResourceItemTest()
+        {
+            var collection = new RDFCollection(RDFModelEnums.RDFItemTypes.Resource);
+            var resource = new RDFResource("https://www.index.hu");
+            collection.AddItem(resource);
+
+            collection.RemoveItem(resource);
+            Assert.Equal(RDFVocabulary.RDF.NIL, collection.ReificationSubject);
+            Assert.Equal(0, collection.ItemsCount);
+        }
+
+        [Fact]
+        public static void RemoveLiteralItemTest()
+        {
+            var collection = new RDFCollection(RDFModelEnums.RDFItemTypes.Literal);
+            var literal = new RDFResource("https://www.index.hu");
+            collection.AddItem(literal);
+
+            collection.RemoveItem(literal);
+            Assert.Equal(RDFVocabulary.RDF.NIL, collection.ReificationSubject);
+            Assert.Equal(0, collection.ItemsCount);
+        }
+
+        [Fact]
+        public static void ClearItemsTest()
+        {
+            var collection = new RDFCollection(RDFModelEnums.RDFItemTypes.Resource);
+            var resource1 = new RDFResource("https://www.index.hu");
+            var resource2 = new RDFResource("https://www.google.com");
+            collection.AddItem(resource1);
+            collection.AddItem(resource2);
+
+            collection.ClearItems();
+            Assert.Equal(RDFVocabulary.RDF.NIL, collection.ReificationSubject);
+            Assert.Equal(0, collection.ItemsCount);
+        }
+        #endregion
     }
 }
