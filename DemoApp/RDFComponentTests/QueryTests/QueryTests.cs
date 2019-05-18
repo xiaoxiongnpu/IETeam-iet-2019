d91fc2a5619419cdd5fb1345e79f0df26268484c
noramurakozy <murakozy.nora@gmail.com>
2019-05-18 12:57:48 +0200

    All query tests added and 1 more triple added to the graph builder

--- DemoApp/RDFComponentTests/QueryTests/QueryTests.cs
+++ DemoApp/RDFComponentTests/QueryTests/QueryTests.cs
@@ -12,122 +12,159 @@ using Xunit.Abstractions;
 
 namespace DemoApp.RDFComponentTests.QueryTests
 {
-    public class FilterTests
+    public class QueryTests
     {
         private RDFGraph graph;
-        private RDFPatternGroup pg1;
         private RDFResource donaldduck;
         private RDFResource goofygoof;
-        private RDFVariable x;
-        private RDFVariable y;
-        private RDFVariable n;
-        private RDFVariable h;
-        private RDFResource dogOf;
-        private RDFPattern n_y_dogOf_x;
-        private RDFPattern y_dogOf_x;
 
-        private readonly ITestOutputHelper output;
-
-        public FilterTests(ITestOutputHelper output)
+        public QueryTests()
         {
             graph = GraphBuilder.WaltDisneyGraphBuild();
             donaldduck = new RDFResource("http://www.waltdisney.com/donald_duck");
             goofygoof = new RDFResource("http://www.waltdisney.com/goofy_goof");
-            x = new RDFVariable("x");
-            y = new RDFVariable("y");
-            n = new RDFVariable("n");
-            h = new RDFVariable("h");
-            pg1 = new RDFPatternGroup("PG1");
-            dogOf = new RDFResource(RDFVocabulary.DC.BASE_URI + "dogOf");
-
-            this.output = output;
         }
 
         [Fact]
         public void CreateVariableTest()
         {
-            x = new RDFVariable("x");
+            //Arrange
+            var x = new RDFVariable("x");
+
+            //Assert
             Assert.Equal("?X", x.VariableName);
         }
 
         [Fact]
         public void CreatePatternGroupTest()
         {
-            pg1 = new RDFPatternGroup("PG1");
+            //Arrange
+            var pg1 = new RDFPatternGroup("PG1");
+
+            //Assert
             Assert.Equal("PG1", pg1.PatternGroupName);
         }
 
         [Fact]
         public void CreateTriplePatternTest()
         {
-            y_dogOf_x = new RDFPattern(y, dogOf, x);
+            //Arrange
+            var x = new RDFVariable("x");
+            var y = new RDFVariable("y");
+            var dogOf = new RDFResource(RDFVocabulary.DC.BASE_URI + "dogOf");
+
+            //Act
+            var y_dogOf_x = new RDFPattern(y, dogOf, x);
+
+            //Assert
             Assert.Equal(x, y_dogOf_x.Object);
         }
 
         [Fact]
         public void CreateQuadruplePatternTest()
         {
-            n_y_dogOf_x = new RDFPattern(n, y, dogOf, x);
+            //Arrange
+            var x = new RDFVariable("x");
+            var y = new RDFVariable("y");
+            var n = new RDFVariable("n");
+            var dogOf = new RDFResource(RDFVocabulary.DC.BASE_URI + "dogOf");
+
+            //Act
+            var n_y_dogOf_x = new RDFPattern(n, y, dogOf, x);
+
+            //Assert
             Assert.Equal(y, n_y_dogOf_x.Subject);
         }
 
         [Fact]
         public void CreateEmptySelectQueryTest()
         {
+            //Arrange
             var selectQuery = new RDFSelectQuery();
+
+            //Assert
             Assert.NotEqual(0, selectQuery.QueryMemberID);
         }
 
         [Fact]
         public void CreateSelectQueryResultTest()
         {
+            //Arrange
             var selectQuery = new RDFSelectQuery();
+
+            //Act
             var selectQueryResult = selectQuery.ApplyToGraph(graph);
+
+            //Assert
             Assert.Equal(0, selectQueryResult.SelectResultsCount);
         }
 
         [Fact]
         public void CreateEmptyAskQueryTest()
         {
-            var askQuery= new RDFAskQuery();
+            //Arrange
+            var askQuery = new RDFAskQuery();
+
+            //Assert
             Assert.NotEqual(0, askQuery.QueryMemberID);
         }
 
         [Fact]
         public void CreateAskQueryResultTest()
         {
+            //Arrange
             var askQuery = new RDFAskQuery();
+
+            //Act
             var askQueryResult = askQuery.ApplyToGraph(graph);
+
+            //Assert
             Assert.False(askQueryResult.AskResult);
         }
 
         [Fact]
         public void CreateEmptyConstructQueryTest()
         {
+            //Arrange
             var constructQuery = new RDFConstructQuery();
+
+            //Assert
             Assert.NotEqual(0, constructQuery.QueryMemberID);
         }
 
         [Fact]
         public void CreateConstructQueryResultTest()
         {
+            //Arrange
             var constructQuery = new RDFConstructQuery();
-            RDFConstructQueryResult constructQueryResult = constructQuery.ApplyToGraph(graph);
+
+            //Act
+            var constructQueryResult = constructQuery.ApplyToGraph(graph);
+
+            //Assert
             Assert.Equal(0, constructQueryResult.ConstructResultsCount);
         }
 
         [Fact]
         public void CreateEmptyDescribeQueryTest()
         {
+            //Arrange
             var describeQuery = new RDFDescribeQuery();
+            
+            //Assert
             Assert.NotEqual(0, describeQuery.QueryMemberID);
         }
 
         [Fact]
         public void CreateDescribeQueryResultTest()
         {
+            //Arrange
             var describeQuery = new RDFDescribeQuery();
+
+            //Act
             var describeQueryResult = describeQuery.ApplyToGraph(graph);
+
+            //Assert
             Assert.Equal(0, describeQueryResult.DescribeResultsCount);
         }
 
@@ -135,109 +172,229 @@ namespace DemoApp.RDFComponentTests.QueryTests
         [Fact]
         public void CreateSameTermFilter()
         {
+            //Arrange
+            var x = new RDFVariable("x");
             var filter = new RDFSameTermFilter(x, donaldduck);
+            var pg1 = new RDFPatternGroup("PG1");
+
+            //Act
             pg1.AddFilter(filter);
+
+            //Assert
             Assert.Contains(filter.ToString(), pg1.ToString());
         }
 
         [Fact]
         public void CreateLangMatchesFilter()
         {
+            //Arrange
+            var n = new RDFVariable("n");
             var filter = new RDFLangMatchesFilter(n, "it-IT");
+            var pg1 = new RDFPatternGroup("PG1");
+
+            //Act
             pg1.AddFilter(filter);
+
+            //Assert
             Assert.Contains(filter.ToString(), pg1.ToString());
         }
 
         [Fact]
         public void CreateComparisonFilter()
         {
+            //Arrange
+            var y = new RDFVariable("y");
             var filter = new RDFComparisonFilter(RDFQueryEnums.RDFComparisonFlavors.LessThan, y, new RDFPlainLiteral("25"));
+            var pg1 = new RDFPatternGroup("PG1");
+
+            //Act
             pg1.AddFilter(filter);
+
+            //Assert
             Assert.Contains(filter.ToString(), pg1.ToString());
         }
 
         [Fact]
         public void CreateIsLiteralFilter()
         {
+            //Arrange
+            var x = new RDFVariable("x");
             var filter = new RDFIsLiteralFilter(x);
+            var pg1 = new RDFPatternGroup("PG1");
+
+            //Act
             pg1.AddFilter(filter);
+
+            //Assert
             Assert.Contains(filter.ToString(), pg1.ToString());
         }
 
         [Fact]
         public void CreateIsNumericFilter()
         {
+            //Arrange
+            var x = new RDFVariable("x");
             var filter = new RDFIsUriFilter(x);
+            var pg1 = new RDFPatternGroup("PG1");
+
+            //Act
             pg1.AddFilter(filter);
+
+            //Assert
             Assert.Contains(filter.ToString(), pg1.ToString());
         }
 
         [Fact]
         public void CreateRegexFilter()
         {
+            //Arrange
+            var n = new RDFVariable("n");
             var filter = new RDFRegexFilter(n, new Regex(@"Mouse", RegexOptions.IgnoreCase));
+            var pg1 = new RDFPatternGroup("PG1");
+
+            //Act
             pg1.AddFilter(filter);
+
+            //Assert
             Assert.Contains(filter.ToString(), pg1.ToString());
         }
 
         [Fact]
         public void AddOrderByModifierDescToQueryTest()
         {
+            //Arrange
+            var x = new RDFVariable("x");
             var query = new RDFSelectQuery();
             var modifier = new RDFOrderByModifier(x, RDFQueryEnums.RDFOrderByFlavors.DESC);
+
+            //Act
             query.AddModifier(modifier);
-            //output.WriteLine(query.ToString() + " " + modifier.ToString());
+
+            //Assert
             Assert.Contains($"order by {modifier}".ToUpper(), query.ToString().ToUpper());
         }
 
         [Fact]
         public void AddOrderByModifierAscToQueryTest()
         {
+            //Arrange
+            var n = new RDFVariable("n");
             var query = new RDFSelectQuery();
             var modifier = new RDFOrderByModifier(n, RDFQueryEnums.RDFOrderByFlavors.ASC);
+
+            //Act
             query.AddModifier(modifier);
+
+            //Assert
             Assert.Contains($"order by {modifier}".ToUpper(), query.ToString().ToUpper());
         }
 
         [Fact]
         public void AddDistinctModifierToQueryTest()
         {
+            //Arrange
             var query = new RDFSelectQuery();
             var modifier = new RDFDistinctModifier();
+
+            //Act
             query.AddModifier(modifier);
+
+            //Assert
             Assert.Contains("distinct".ToUpper(), query.ToString().ToUpper());
         }
 
         [Fact]
         public void AddLimitModifierToQueryTest()
         {
+            //Arrange
             var query = new RDFSelectQuery();
             var modifier = new RDFLimitModifier(100);
+
+            //Act
             query.AddModifier(modifier);
+
+            //Assert
             Assert.Contains(modifier.ToString(), query.ToString());
         }
 
         [Fact]
         public void AddOffsetModifierToQueryTest()
         {
+            //Arrange
             var query = new RDFSelectQuery();
             var modifier = new RDFOffsetModifier(25);
+
+            //Act
             query.AddModifier(modifier);
+
+            //Assert
             Assert.Contains(modifier.ToString(), query.ToString());
         }
 
-        //<--23
+        [Fact]
+        public void SelectQueryResultTest()
+        {
+            //Arrange
+            var x = new RDFVariable("x");
+            var q = new RDFSelectQuery();
+            var patternGroup = new RDFPatternGroup("PatternGroup");
+            patternGroup.AddPattern(new RDFPattern(x, RDFVocabulary.FOAF.AGE, new RDFTypedLiteral("85", RDFModelEnums.RDFDatatypes.XSD_INTEGER)));
+            q.AddPatternGroup(patternGroup);
+
+            //Act
+            var selectResult = q.ApplyToGraph(graph);
+
+            //Assert
+            Assert.Equal(2, selectResult.SelectResultsCount);
+        }
 
         [Fact]
-        public void AddPatternToPatternGroupTest()
+        public void AskQueryResultTrueTest()
         {
-            RDFSelectQuery q = new RDFSelectQuery();
-            pg1.AddPattern(new RDFPattern(x, RDFVocabulary.FOAF.AGE, new RDFPlainLiteral("85")));
-            q.AddPatternGroup(pg1);
+            //Arrange
+            var y = new RDFVariable("y");
+            var askQuery = new RDFAskQuery();
+            var patternGroup = new RDFPatternGroup("PatternGroup");
+            patternGroup.AddPattern(new RDFPattern(donaldduck, y, new RDFTypedLiteral("85", RDFModelEnums.RDFDatatypes.XSD_INTEGER)));
+            askQuery.AddPatternGroup(patternGroup);
 
-            RDFSelectQueryResult selectResult = q.ApplyToGraph(graph);
-            output.WriteLine(selectResult.SelectResults.Rows.Count.ToString());
-            //output.WriteLine(graph.TriplesCount.ToString());
+            //Act
+            var result = askQuery.ApplyToGraph(graph);
+
+            //Assert
+            Assert.True(result.AskResult);
         }
+
+        [Fact]
+        public void AskQueryResultFalseTest()
+        {
+            //Arrange
+            var y = new RDFVariable("y");
+            var askQuery = new RDFAskQuery();
+            var patternGroup = new RDFPatternGroup("PatternGroup");
+            patternGroup.AddPattern(new RDFPattern(donaldduck, y, new RDFTypedLiteral("90", RDFModelEnums.RDFDatatypes.XSD_INTEGER)));
+            askQuery.AddPatternGroup(patternGroup);
+
+            //Act
+            var result = askQuery.ApplyToGraph(graph);
+
+            //Assert
+            Assert.False(result.AskResult);
+        }
+
+        [Fact]
+        public void DescribeQueryResultTest()
+        {
+            //Arrange
+            var query = new RDFDescribeQuery();
+
+            //Act
+            query.AddDescribeTerm(new RDFResource("http://www.waltdisney.com/donald_duck"));
+            var result = query.ApplyToGraph(graph);
+
+            //Assert
+            Assert.Equal(2,result.DescribeResultsCount);
+        }
+
     }
 }
